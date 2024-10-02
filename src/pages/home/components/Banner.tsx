cafcdc274d89912aaebd5f09b73d41bc99dbafa2
Bapdev <nguyenduybach12a11@gmail.com>
2024-10-03 06:08:02 +0700

    animation

--- src/pages/home/components/Banner.tsx
+++ src/pages/home/components/Banner.tsx
@@ -1,8 +1,24 @@
 import { useNavigate } from 'react-router-dom'
+import { motion } from "framer-motion"
 
 import { Button } from '../../../components/Button'
 import { BannerType } from '../../../utils/types'
 import { useHomeContext } from '../context/HomeContext'
+
+const variantBanner = {
+  initial: {
+    y: -100,
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+}
 
 export const Banner = ({ id, name, overview, poster, backdrop }: BannerType) => {
   const { setIsOpenDialogTrailer, setIdBannerSelected } = useHomeContext();
@@ -22,16 +38,16 @@ export const Banner = ({ id, name, overview, poster, backdrop }: BannerType) => 
          style={{backgroundImage: `url(https://image.tmdb.org/t/p/original/${ backdrop })`}}
     >
         <div className="max-w-screen-2xl z-10 h-fit flex items-center justify-between">
-          <div className='w-full lg:w-2/3 px-4'>
-              <h2 className="font-bold text-4xl md:text-6xl lg:text-8xl delay-300 text-white transition duration-700 ease-in-out opacity-100 translate-y-0">{ name }</h2>
-              <p className="font-medium text-white text-xs md:text-xl my-12 delay-[600ms] transition duration-700 ease-in-out opacity-100 translate-y-0">{ overview }</p>
-              <div className="flex delay-[900ms] text-white transition duration-700 ease-in-out opacity-100 translate-y-0">
+          <motion.div className='w-full lg:w-2/3 px-4' variants={variantBanner} initial="initial" whileInView="animate">
+              <motion.h2 className="font-bold text-4xl md:text-6xl lg:text-8xl delay-300 text-white transition duration-700 ease-in-out opacity-100 translate-y-0" variants={variantBanner}>{ name }</motion.h2>
+              <motion.p className="font-medium text-white text-xs md:text-xl my-12 delay-[600ms] transition duration-700 ease-in-out opacity-100 translate-y-0" variants={variantBanner}>{ overview }</motion.p>
+              <motion.div className="flex delay-[900ms] text-white transition duration-700 ease-in-out opacity-100 translate-y-0" variants={variantBanner}>
                   <Button text='Watch Now' size='lg' type='primary' className='mr-4' onClick={handleWatchNow} />
                   <Button text='Watch trailer' size='lg' ghost onClick={handleWatchTrailer} />
-              </div>
-          </div>
+              </motion.div>
+          </motion.div>
           <div className='hidden px-4 lg:block lg:w-1/3'>
-            <img className="w-96 rounded-3xl animate-scale" src={`https://image.tmdb.org/t/p/w500/${ poster }`} alt="Poster" />
+            <img className="w-96 rounded-3xl scale-animate" src={`https://image.tmdb.org/t/p/w500/${ poster }`} alt="Poster" />
           </div>
         </div>
     </div>
