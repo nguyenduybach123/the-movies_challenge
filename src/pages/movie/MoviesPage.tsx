cb79c9e17201a84f110b9d8aa9a7fe21460467fa
Bapdev <nguyenduybach12a11@gmail.com>
2024-10-03 06:18:47 +0700

    animation v2

--- src/pages/movie/MoviesPage.tsx
+++ src/pages/movie/MoviesPage.tsx
@@ -74,7 +74,7 @@ export const MoviesPage = () => {
 
   return (
     <>
-      <div className="relative h-48 bg-[url(&quot;src/assets/footer-bg.jpg&quot;)] bg-cover bg-center bg-no-repeat after:content-[&quot;&quot;] after:absolute after:bottom-0 after:left-0 after:right-0 after:top-0 after:bg-gradient-to-t after:from-black-main after:to-transparent">
+      <div className="relative h-48 after:content-[''] after:absolute after:bottom-0 after:left-0 after:right-0 after:top-0 after:bg-gradient-to-t after:from-black-main after:to-white">
         <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 md:translate-y-0 text-white text-4xl font-bold z-10">Movies</span>
       </div>
       <div className="bg-black-main px-8 py-4 md:px-16 md:py-8">
